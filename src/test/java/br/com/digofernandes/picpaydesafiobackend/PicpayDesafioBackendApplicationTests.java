94e5aaaa4b8cd46a720683c5457411505ea5cafc
DigoFernandes <digofernandes017@gmail.com>
2024-03-20 22:00:28 -0300

    first commit

---
+++ src/test/java/br/com/digofernandes/picpaydesafiobackend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.digofernandes.picpaydesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+
+@EnableJdbcAuditing
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
