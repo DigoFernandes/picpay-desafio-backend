25cb00be89ab8e23008a66f99d8622e20899bbac
DigoFernandes <digofernandes017@gmail.com>
2024-03-25 23:29:13 -0300

    Authorization

---
+++ src/main/java/br/com/digofernandes/picpaydesafiobackend/authorization/Authorization.java
@@ -0,0 +1,5 @@
+package br.com.digofernandes.picpaydesafiobackend.authorization;
+
+public record Authorization() {
+    
+}
--- src/main/java/br/com/digofernandes/picpaydesafiobackend/authorization/AuthorizerService.java
+++ src/main/java/br/com/digofernandes/picpaydesafiobackend/authorization/AuthorizerService.java
@@ -1,13 +1,24 @@
 package br.com.digofernandes.picpaydesafiobackend.authorization;
 
+import br.com.digofernandes.picpaydesafiobackend.authorization.Authorization;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 import br.com.digofernandes.picpaydesafiobackend.transaction.Transaction;
 
 @Service
 public class AuthorizerService {
+    private RestClient restClient;
+
+    public AuthorizerService(RestClient.Builder builder) {
+        this.restClient = builder
+            .baseUrl("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc")
+            .build();
+    }
 
     public void authorize(Transaction transaction) {
-        
-    }
+        var response = restClient.get()
+            .retrieve()
+            .toEntity(Authorization.class);    
+        }
 }
