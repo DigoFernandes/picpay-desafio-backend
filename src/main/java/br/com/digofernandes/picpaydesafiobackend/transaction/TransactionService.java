410e21d3fc41cbfad97b0fac806decb6195dcd15
DigoFernandes <digofernandes017@gmail.com>
2024-03-25 23:12:19 -0300

    creating a validate method

--- src/main/java/br/com/digofernandes/picpaydesafiobackend/transaction/TransactionService.java
+++ src/main/java/br/com/digofernandes/picpaydesafiobackend/transaction/TransactionService.java
@@ -2,29 +2,49 @@ package br.com.digofernandes.picpaydesafiobackend.transaction;
 
 import org.springframework.stereotype.Service;
 
+import br.com.digofernandes.picpaydesafiobackend.wallet.Wallet;
 import br.com.digofernandes.picpaydesafiobackend.wallet.WalletRepository;
+import br.com.digofernandes.picpaydesafiobackend.wallet.WalletType;
 
 @Service
 public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final WalletRepository walletRepository;
 
-
     public TransactionService(TransactionRepository transactionRepository, WalletRepository walletRepository) {
         this.transactionRepository = transactionRepository;
         this.walletRepository = walletRepository;
     }
+
     public Transaction create(Transaction transaction) {
-        //1-validar
-        
-        //2 - criar a transação
+        // 1-validar
+        validate(transaction);
+        // 2 - criar a transação
         var newTransaction = transactionRepository.save(transaction);
-        //3 - debitar da carteira
+        // 3 - debitar da carteira
 
         var wallet = walletRepository.findById(transaction.payer()).get();
         walletRepository.save(wallet.debit(transaction.value()));
         // 4 - chamar serviços externos
 
         return newTransaction;
+    }
+
+    /*
+     * - the payer has a common wallet
+     * - the payer has enough balance
+     * - the payer cannot be the receiver
+     */
+    private void validate(Transaction transaction) {
+    walletRepository.findById(transaction.payee())
+        .map(payee -> walletRepository.findById(transaction.payer())
+            .map(payer -> isTransactionValid(transaction, payer) ? transaction : null)
+        );
+}
+
+    private boolean isTransactionValid(Transaction transaction, Wallet payer) {
+        return payer.type() == WalletType.COMUM.getValue() &&  
+            payer.balance().compareTo(transaction.value()) >= 0 &&
+            !payer.id().equals(transaction.payee());
     }
 }
